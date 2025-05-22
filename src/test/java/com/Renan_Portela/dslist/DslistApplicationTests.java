52602a63e71979ee88d94d6cb12ff9e355a4989c
Renan Portela <renanportela@protonmail.com>
2025-05-22 17:27:29 -0300

    Project created

---
+++ src/test/java/com/Renan_Portela/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Renan_Portela.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
